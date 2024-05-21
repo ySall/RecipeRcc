389b553a1aa070075cb15de9b0952b15be387dbb
ySall <125202482+ySall@users.noreply.github.com>
2024-05-21 23:52:20 +0700

    Modify login and register

--- app/src/main/java/com/example/recipe/LogIn.kt
+++ app/src/main/java/com/example/recipe/LogIn.kt
@@ -1,26 +1,148 @@
 package com.example.recipe
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.recipe.databinding.ActivityLoginBinding
-import com.example.recipe.databinding.ActivitySplashBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
 
 class LogIn : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_login)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Login -> Register
-        binding.btnRegister.setOnClickListener{
+        // Initialize SharedPreferences
+        sharedPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE)
+        checkSavedCredentials()
+
+        binding.btnRegister.setOnClickListener {
             startActivity(Intent(this@LogIn, Register::class.java))
         }
-        // Login -> Onboarding
-        binding.btnLogIn.setOnClickListener{
-            startActivity(Intent(this@LogIn, Onboarding::class.java))
+
+        binding.forgetPassword.setOnClickListener {
+            startActivity(Intent(this@LogIn, ForgotPassword::class.java))
         }
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("961470276131-jp89r20uo7ibkm9no4ili4orgicrl0pa.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        binding.btnLogIn.setOnClickListener {
+            val email = binding.usernameInput.text.toString()
+            val password = binding.passwordInput.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    Toast.makeText(this, "Invalid email", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            if (firebaseAuth.currentUser!!.isEmailVerified) {
+                                if (binding.checkbox.isChecked) {
+                                    saveCredentials(email, password)
+                                } else {
+                                    clearCredentials()
+                                }
+                                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                                startActivity(Intent(this@LogIn, Onboarding::class.java))
+                            } else {
+                                Toast.makeText(this, "Please verify your email address", Toast.LENGTH_SHORT).show()
+                            }
+                        } else {
+                            Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.btnGoogle.setOnClickListener {
+            signInWithGoogle()
+        }
+    }
+
+    private fun saveCredentials(email: String, password: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString("email", email)
+        editor.putString("password", password)
+        editor.apply()
+    }
+
+    private fun clearCredentials() {
+        val editor = sharedPreferences.edit()
+        editor.remove("email")
+        editor.remove("password")
+        editor.apply()
+    }
+
+    private fun checkSavedCredentials() {
+        val savedEmail = sharedPreferences.getString("email", "")
+        val savedPassword = sharedPreferences.getString("password", "")
+        if (!savedEmail.isNullOrEmpty() && !savedPassword.isNullOrEmpty()) {
+            binding.usernameInput.setText(savedEmail)
+            binding.passwordInput.setText(savedPassword)
+            binding.checkbox.isChecked = true
+        }
+    }
+
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign-In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign-In failed
+                Toast.makeText(this, "Google Sign-In failed", Toast.LENGTH_SHORT).show()
+                Log.e("Login with google", e.message ?: "")
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success
+                    val user = firebaseAuth.currentUser
+                    Toast.makeText(this, "Google Sign-In successful", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this@LogIn, Onboarding::class.java))
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this, "Google Sign-In failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
     }
 }
--- app/src/main/java/com/example/recipe/Register.kt
+++ app/src/main/java/com/example/recipe/Register.kt
@@ -1,22 +1,27 @@
 package com.example.recipe
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.recipe.databinding.ActivityRegisterBinding
 import com.example.recipe.databinding.ActivityRegisterBinding.inflate
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 
 
 class Register : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_register)
+
         binding = inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Set click listener for textRegister
+        // Set click listener for textRegoster
         binding.textRegister.setOnClickListener{
             navigateToLogin()
         }
@@ -25,14 +30,48 @@ class Register : AppCompatActivity() {
             navigateToLogin()
         }
 
-        binding.btnRegister.setOnClickListener{
-            startActivity(Intent(this@Register, LogIn::class.java))
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnRegister.setOnClickListener {
+            val email = binding.usernameInput.text.toString()
+            val password = binding.passwordInput.text.toString()
+            val confirmPassword = binding.confirmPasswordInput.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    Toast.makeText(this, "Invalid email", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+                if (password == confirmPassword){
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                firebaseAuth.currentUser?.sendEmailVerification()
+                                    ?.addOnCompleteListener { verificationTask ->
+                                        if (verificationTask.isSuccessful) {
+                                            Toast.makeText(this, "Verification email sent. Please verify your email address.", Toast.LENGTH_SHORT).show()
+                                        } else {
+                                            Toast.makeText(this, "Failed to send verification email", Toast.LENGTH_SHORT).show()
+                                        }
+                                    }
+                                Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+                                navigateToLogin()
+                            } else {
+                                if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                                    Toast.makeText(this, "Invalid email", Toast.LENGTH_SHORT).show()
+                                } else {
+                                    Toast.makeText(this, "Registration failed", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        }
+                } else {
+                    Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Empty field is not allowed!", Toast.LENGTH_SHORT).show()
+            }
         }
-
     }
 
-    private fun navigateToLogin(){
-        // create intent
+    private fun navigateToLogin() {
         val intent = Intent(this, LogIn::class.java)
 
         startActivity(intent)
